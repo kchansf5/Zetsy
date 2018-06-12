e3eceea6c3520783eafca256feef72a79d9b03b3
Ken <kchansf5@gmail.com>
2018-06-12 14:09:31 -0700

    Start backend for shopping cart items

--- app/controllers/api/shopping_cart_item_controller.rb
+++ app/controllers/api/shopping_cart_item_controller.rb
@@ -1,2 +1,40 @@
 class Api::ShoppingCartItemsController < ApplicationController
+
+  def index
+    @shopping_cart_items = ShoppingCartItem.all
+  end
+
+  def show
+    @shopping_cart_item = ShoppingCartItem.find(params[:id])
+  end
+
+  def create
+    @shopping_cart_item = ShoppingCartItem.new(shopping_cart_item_params)
+
+    if @shopping_cart_item.save
+      render :show
+    else
+      render json: @shopping_cart_item.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @shopping_cart_item = ShoppingCartItem.find(params[:id])
+    if @shopping_cart_item.update(shopping_cart_item_params)
+      render json: @shopping_cart_item
+    else
+      render json: @shopping_cart_item.errors.full_messages, status: 422
+  end
+
+  def destroy
+    @shopping_cart_item = current_user.ShoppingCartItem.find(params[:id])
+    @shoppingcart_item.destroy
+    render :show
+  end
+
+  private
+
+  def shopping_cart_item_params
+    params.require(:shoppingCartItem).permit(:product_id, :user_id, :quantity)
+  end
 end
--- app/models/shopping_cart_item.rb
+++ app/models/shopping_cart_item.rb
@@ -1,5 +1,6 @@
 class ShoppingCartItem < ApplicationRecord
   validates :product_id, :user_id, :quantity, presence: true
 
-  has_many :products
+  belongs_to :user
+  belongs_to :product
 end
