1b0da4c468a24a67cb01868c372aed8a9200fdb6
Ken <kchansf5@gmail.com>
2018-06-13 09:17:50 -0700

    Create cartReducer for shopping cart items

---
+++ frontend/reducers/shopping_cart_items_reducer.js
@@ -0,0 +1,25 @@
+import merge from 'lodash/merge';
+
+import {
+  RECEIVE_ALL_ITEMS,
+  RECEIVE_ITEM,
+  REMOVE_ITEM } from '../actions/shopping_cart_item_actions';
+
+const cartReducer = (state = {}, action) => {
+  Object.freeze(state);
+  switch(action.type) {
+    case RECEIVE_ALL_ITEMS:
+      return action.items;
+    case RECEIVE_ITEM:
+      const newItem = action.payload.item;
+      return merge({}, state, { [newItem.id]: newItem});
+    case REMOVE_ITEM:
+      let newState = merge({}, state);
+      delete newState[action.item];
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default cartReducer;
