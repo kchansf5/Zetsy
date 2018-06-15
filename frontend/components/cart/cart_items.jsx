d131bbc09e6a215ed8ddcc6ed67d5497067d4b41
Ken <kchansf5@gmail.com>
2018-06-15 02:20:03 -0700

    Add styling to shopping cart

--- frontend/components/cart/cart_items.jsx
+++ frontend/components/cart/cart_items.jsx
@@ -40,67 +40,73 @@ class CartItems extends React.Component {
     if (this.props.items.length > 0) {
     return (
       <div className="shopping-cart">
-        <div className="num-items">{this.props.items.length} item(s) in your cart</div>
-        {this.props.items.map(item => {
-          const subtotal = item.price * item.quantity;
-          subtotals.push(subtotal);
-          return (
-            <div>
-              <div key={item.id} className="cart-items">
 
-                  <div className="single-item">
-                    <Link to={`/products/${item.product_id}`}>
-                      <img className="cart-item-img" src={item.image_url}/>
-                    </Link>
+        <div className="top-of-cart">
+          <div className="num-items">{this.props.items.length} item(s) in your cart</div>
+          <Link to="/products"><button>Keep Shopping</button></Link>
+        </div>
 
-                    <div className="col2">
-                      <div className="cart-item-name">{item.name}</div>
-                      <button className="remove" onClick={()=>this.props.deleteItem(item.id)}>Remove</button>
+            <div className="main-cart">
+              <div className="all-items">
+                    {this.props.items.map(item => {
+                      const subtotal = item.price * item.quantity;
+                      subtotals.push(subtotal);
+                      return (
+                          <div key={item.id} className="single-item">
+
+
+                          <Link to={`/products/${item.product_id}`}>
+                            <img className="cart-item-img" src={item.image_url}/>
+                          </Link>
+
+                          <div className="col2">
+                            <div className="cart-item-name">{item.name}</div>
+                            <button className="remove" onClick={()=>this.props.deleteItem(item.id)}>Remove</button>
+                          </div>
+
+                          <div className="col3">
+                            <select className="cart-item-quantity" value={item.quantity}
+                              id="quantity_dropdown" className="dropdown"
+                              onChange={this.updateQuantity(item)}>
+                              <option value="1">1</option>
+                              <option value="2">2</option>
+                              <option value="3">3</option>
+                              <option value="4">4</option>
+                              <option value="5">5</option>
+                              <option value="6">6</option>
+                              <option value="7">7</option>
+                              <option value="8">8</option>
+                              <option value="9">9</option>
+                              <option value="10">10</option>
+                              <option value="11">11</option>
+                              <option value="12">12</option>
+                              <option value="13">13</option>
+                              <option value="14">14</option>
+                              <option value="15">15</option>
+                              <option value="16">16</option>
+                              <option value="17">17</option>
+                              <option value="18">18</option>
+                              <option value="19">19</option>
+                              <option value="20">20</option>
+                            </select>
+                          </div>
+
+                          <div className="col4">
+                            <div className="subtotal">${subtotal}.00</div>
+                            <div className="item-price">(${item.price}.00 each)</div>
+                          </div>
+
                     </div>
-
-                    <div className="col3">
-                      <select className="cart-item-quantity" value={item.quantity}
-                        id="quantity_dropdown" className="dropdown"
-                        onChange={this.updateQuantity(item)}>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
-                        <option value="11">11</option>
-                        <option value="12">12</option>
-                        <option value="13">13</option>
-                        <option value="14">14</option>
-                        <option value="15">15</option>
-                        <option value="16">16</option>
-                        <option value="17">17</option>
-                        <option value="18">18</option>
-                        <option value="19">19</option>
-                        <option value="20">20</option>
-                      </select>
-                    </div>
-
-                    <div className="col4">
-                      <div className="subtotal">${subtotal}.00</div>
-                      <div className="item-price">(${item.price}.00 each)</div>
-                    </div>
-
-                </div>
-
-
-
+                  );
+                })}
               </div>
-              <div className="total">Item(s) total ${subtotals.reduce((acc, el)=> acc + el)}.00</div>
-          </div>
-          );
-        })}
-        <button onClick={()=>this.props.deleteAllItems()}>Checkout</button>
-        <Link to="/products"><button>Keep Shopping</button></Link>
+
+              <div className="checkout">
+                <div className="total">Item(s) total ${subtotals.reduce((acc, el)=> acc + el)}.00</div>
+                <button onClick={()=>this.props.deleteAllItems()}>Checkout</button>
+              </div>
+
+            </div>
       </div>
 
     );
