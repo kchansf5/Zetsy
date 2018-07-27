a404c025ffb9ebfaf6c56329bd99279cffa9d64e
Ken <kchansf5@gmail.com>
2018-07-27 14:21:43 -0700

    Add pg_search_scope to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,14 @@
 class Product < ApplicationRecord
+  include PgSearch
+  pg_search_scope :search_name,
+                  :against =>
+                  [:product_name, :description, :category],
+                  :using => {
+                    :tsearch => {
+                      :prefix => true,
+                      :any_word => true,
+                    }
+                  }
   validates :product_name, :description, :price, :category, :image_url, presence: true
 
   has_many :shopping_cart_items
