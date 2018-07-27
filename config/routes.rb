2dc336c0f8ab9321c218c221b8a141aee82b0fd1
Ken <kchansf5@gmail.com>
2018-07-27 14:37:55 -0700

    Add search to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   delete 'api/shopping_cart_item/checkout' => 'api/shopping_cart_item#checkout'
-  
+
   namespace :api, defaults: {format: :json} do
     resource :user, only: [:create]
     resource :session, only: [:create, :destroy, :show]
     resources :products, only: [:index, :show]
     resources :shopping_cart_item, only: [:index, :show, :create, :update, :destroy]
+    resources :search, only: [:index]
   end
 
 
